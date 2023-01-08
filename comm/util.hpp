072d0c7a45aca441f7f47c1b06267620235d8732
zevin02 <1833195090@qq.com>
2023-01-08 17:15:32 +0800

    compiler_run 完成了start模块，编写了文件的读写，状态码描述，形成唯一的文件名

--- comm/util.hpp
+++ comm/util.hpp
@@ -7,8 +7,33 @@ using namespace std;
 #include <sys/types.h>
 #include <sys/time.h>
 #include <time.h>
+#include<fstream>
+#include<atomic>
 namespace ns_util
 {
+    class TimeUtil
+    {
+    public:
+        static string GetTime() // 获得时间
+        {
+            struct timeval _time;
+            gettimeofday(&_time, nullptr);
+            struct tm tm = *localtime(&_time.tv_sec);
+            static char time_str[32]{0};
+            snprintf(time_str, sizeof(time_str),
+                     "%04d-%02d-%02d %02d:%02d:%02d",
+                     tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+            return string(time_str);
+        }
+        // 获得毫秒级时间戳
+        static string GetTimeMs()
+        {
+            struct timeval _time;
+            gettimeofday(&_time, nullptr);
+            // 秒-》1000ms
+            return to_string(_time.tv_sec * 1000 + _time.tv_usec / 1000); //
+        }
+    };
     string temp = "./temp/";
 
     class PathUtil
@@ -72,34 +97,47 @@ namespace ns_util
                 return false; // 获取属性失败，文件不存在
             }
         }
-        static string UniqueFilename(const string path)
+        static string UniqueFilename()//这样就形成了一个唯一的文件名,毫秒即时间戳+uid原子性自增
         {
+            string ms = TimeUtil::GetTimeMs();
+            static atomic<uint64_t> id(0);
+            string uid= to_string(id);
+            id++;//这个就是一个原子性的自增
+            return ms+"_"+uid;
 
         }
-        static bool WriteFile(const string & filename,const string& code)
+        static bool WriteFile(const string &filename, const string &source)
         {
-            
+            ofstream ofs(filename,ios::out|ios::binary);
+            if(!ofs.is_open())
+            {
+                //没有被打开成功
+                ofs.close();
+                return false;
+            }
+            ofs.write(source.c_str(),source.size());
+
+            ofs.close();
+        }
+
+        static bool ReadFile(const string &filename,string & content,bool keep=false)
+        {
+            ifstream ifs(filename,ios::in|ios::binary);
+            if(!ifs.is_open())
+            {   
+                ifs.close();
+                return false;
+            }
+            string line;
+            while(getline(ifs,line))
+            {
+                //getline不会保留换行所以如果使用这个的话，所有的换行我们都不会保留
+                content+=line;
+                content+=(keep?"\n":"");//如果上层需要这个换行我们就需要保留
+            }
+
+            return true;
         }
     };
 
-    class TimeUtil
-    {
-    public:
-        static string GetTime()
-        {
-            struct timeval _time;
-            gettimeofday(&_time, nullptr);
-            struct tm tm = *localtime(&_time.tv_sec);
-            static char time_str[32]{0};
-            snprintf(time_str, sizeof(time_str),
-                     "%04d-%02d-%02d %02d:%02d:%02d",
-                     tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-            return string(time_str);
-        }
-    };
-    class  JsonUtil
-    {
-        public:
-        static void Serialize()
-    };
 };
--- compile_server/compile_run.hpp
+++ compile_server/compile_run.hpp
@@ -1,51 +1,144 @@
 #pragma once
-#include"compiler.hpp"
-#include"runner.hpp"
+#include "compiler.hpp"
+#include "runner.hpp"
 using namespace ns_compiler;
 using namespace ns_runner;
 
-//编译并运行的功能
-//适配用户需求
-#include<jsoncpp/json/json.h>
+// 编译并运行的功能
+// 适配用户需求
+#include <signal.h>
+#include <jsoncpp/json/json.h>
 
 namespace ns_compile_run
 {
     class CompileRun
     {
-        public:
-        //injson
-        //code：用户给自己提交的代码
-        //input：用户给自己提交的代码对应的输入
-        //cpulimit:时间限制
-        //memlimit：空间限制
+    public:
+        // injson
+        // code：用户给自己提交的代码
+        // input：用户给自己提交的代码对应的输入
+        // cpulimit:时间限制
+        // memlimit：空间限制
 
-        //outjson
-        //status:状态码
-        //reason:请求结果，原因
-        //选填写
-        //stdout:输出结果
-        //stderr：程序运行完的错误结果
-        static void Start(const string& injson,string& outjson)
+        // outjson
+        // status:状态码
+        // reason:请求结果，原因
+        // 选填写
+        // stdout:输出结果
+        // stderr：程序运行完的错误结果
+
+        static string CodeToDesc(int code, const string &filename)
+        {
+            // code>0信号导致崩溃
+            // code<0编译运行时出现的内部错误
+            // code=0整个过程完成
+            string desc;
+            switch (code)
+            {
+            case 0:
+                desc = "编译运行成功";
+                break;
+            case -1:
+                desc = "用户提交了空代码";
+                break;
+            case -2:
+                desc = "未知错误";
+                break;
+            case -3:
+                //
+                FileUtil::ReadFile(PathUtil::Compile_Error(filename), desc, true);
+                break;
+            case SIGABRT: // 6
+                desc = "内存超过范围";
+                break;
+            case SIGXCPU: // 24
+                desc = "CPU使用超时";
+                break;
+            case SIGFPE: // 8
+                desc = "浮点数溢出,除0错误";
+                break;
+            default:
+                desc = "未知信号,for debug status_code=" + to_string(code);
+                break;
+            }
+            return desc;
+        }
+        static void Start(const string &injson, string &outjson)
         {
             Json::Value in_value;
             Json::Reader reader;
-            reader.parse(injson,in_value);
-            string code=in_value["code"].asString();//用户提交的代码
-            string input=in_value["input"].asString();//用户的输入
-            int cpulimit=in_value["cpulimit"].asInt();
-            int memlimit=in_value["memlimit"].asInt();
+            reader.parse(injson, in_value);
+            string code = in_value["code"].asString();   // 用户提交的代码
+            string input = in_value["input"].asString(); // 用户的输入
+            int cpulimit = in_value["cpulimit"].asInt();
+            int memlimit = in_value["memlimit"].asInt();
 
-            if(code.size()==0)
+            Json::Value out_value;
+            int status_code = 0;
+            int ret = 0;
+            // 错误码为负数说明这个就是由位置错误导致的
+            string file_name; // 需要内部形成的唯一文件名
+
+            if (code.size() == 0)
             {
-                //差错处理
+                status_code = -1;
+                goto end;
+                // 差错处理
             }
-            //形成一个唯一的文件名
 
-            string file_name=FileUtil::UniqueFilename();
-            FileUtil::WriteFile(PathUtil::Src(file_name),code);//往唯一文件中写入code代码
-            Compiler::Compile(file_name);
-            Runner::Run(file_name,cpulimit,memlimit);
+            // 形成一个唯一的文件名
+            // 使用毫秒级时间戳+原子性递增的方式来形成一个唯一值
+
+            file_name = FileUtil::UniqueFilename();
+            if (!FileUtil::WriteFile(PathUtil::Src(file_name), code))
+            {
+                // 往唯一文件中写入code代码
+                status_code = -2;
+                goto end;
+            }
+
+            if (!Compiler::Compile(file_name))
+            {
+                status_code = -3;
+                goto end;
+                // 编译失败的信息就需要进行返回
+            }
+            ret = Runner::Run(file_name, cpulimit, memlimit);
+            if (ret < 0)
+            {
+                status_code = -2;
+                goto end;
+            }
+            // 错误码为正数代表是由于信号导致的出错
+            else if (ret > 0)
+            {
+                status_code = ret;
+                goto end;
+            }
+            // 错误码为0代表就是正常退出
+            else
+            {
+                // 运行成功
+                status_code = 0;
+                goto end;
+            }
+
+        end:
+            out_value["status"] = status_code;                        // 未知错误
+            out_value["reason"] = CodeToDesc(status_code, file_name); //
+            if (status_code == 0)
+            {
+                // 整个过程全部成功
+                string string_stdout;
+                FileUtil::ReadFile(PathUtil::Stdout(file_name), string_stdout, true);
+                out_value["stdout"] = string_stdout;
+                string string_stderr;
+                FileUtil::ReadFile(PathUtil::StdError(file_name), string_stderr, true); // 在文件中
+                out_value["stderr"] = string_stderr;                                    //
+            }
+
+            Json::FastWriter writer;
+            outjson = writer.write(out_value);
         }
     };
 };
-
